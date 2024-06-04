724b1aa02c09c7281b6d53b2681bcb2d7d05fb5c
USER <shpbd1@gmail.com>
2024-06-04 11:38:45 +0900

    jjjjjjjjjjj

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -14,7 +14,9 @@ import com.example.myapplication.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
 
+
     
+
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
